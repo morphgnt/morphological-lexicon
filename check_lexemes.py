bf95b7d06dd2965ed16c704903a5e9ce45046332
James Tauber <jtauber@jtauber.com>
2013-11-04 20:51:50 +0800

    initial code to check lexemes

---
+++ check_lexemes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from morphgnt import filesets
+from morphgnt.utils import load_yaml
+
+lexemes = load_yaml("lexemes.yaml")
+fs = filesets.load("filesets.yaml")
+
+total = 0
+match = 0
+first_fail = None
+
+for row in fs["sblgnt-test"].rows():
+    total += 1
+    if row["lemma"].decode("utf-8") in lexemes:
+        match += 1
+    elif first_fail is None:
+        first_fail = "{} {} {}".format(row["lemma"], row["ccat-pos"], row["robinson"])
+
+print "{}/{} = {}%".format(match, total, int(1000 * match / total) / 10)
+if first_fail:
+    print first_fail
--- main.py
+++
@@ -1,7 +0,0 @@
-from morphgnt import filesets
-
-
-fs = filesets.load("filesets.yaml")
-
-for row in fs["sblgnt-test"].rows():
-    print row
