1eb2efa1ef3acb049e46c728d26ed2820978b5a2
James Tauber <jtauber@jtauber.com>
2013-11-09 02:27:43 +0800

    setup.py for morphgnt module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "morphgnt-tools",
+    version = "0.1dev",
+    license = "MIT",
+    author = "James Tauber",
+    author_email = "jtauber@jtauber.com",
+    py_modules = ["morphgnt"],
+)
