29ce578d96fe9642f84e06cccbc11f7a06014b7e
James Tauber <jtauber@jtauber.com>
2013-11-14 14:17:53 +0800

    fail yaml load gracefully if file empty

--- morphgnt/utils.py
+++ morphgnt/utils.py
@@ -5,5 +5,5 @@ def load_yaml(filename, wrapper=lambda key, metadata: metadata):
     with open(filename) as f:
         return {
             key: wrapper(key, metadata)
-            for key, metadata in yaml.load(f).items()
+            for key, metadata in (yaml.load(f) or {}).items()
         }
