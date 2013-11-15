a8cc485856de9bfa566fb763dbe19b2253a564e1
James Tauber <jtauber@jtauber.com>
2013-11-16 02:18:03 +0800

    added wrapper around form list

--- scripts/generate_forms.py
+++ scripts/generate_forms.py
@@ -20,38 +20,38 @@ for row in fs["sblgnt-forms"].rows():
         if lexeme["pos"] in ["RA", "A", "N", "RR"]:
             gender = row["ccat-parse"][6]
             case_number = row["ccat-parse"][4:6]
-            form_list = forms.setdefault(lemma, {}).setdefault(gender, {}).setdefault(case_number, [])
+            form_list = forms.setdefault(lemma, {}).setdefault(gender, {}).setdefault(case_number, {}).setdefault("forms", [])
             if {"form": form} not in form_list:
                 form_list.append({"form": form})
         elif lexeme["pos"] in ["RP1"]:
             case_number = row["ccat-parse"][4:6]
-            form_list = forms.setdefault(lemma, {}).setdefault(case_number, [])
+            form_list = forms.setdefault(lemma, {}).setdefault(case_number, {}).setdefault("forms", [])
             if {"form": form} not in form_list:
                 form_list.append({"form": form})
         elif lexeme["pos"] in ["V"]:
             mood = row["ccat-parse"][3]
             if mood in ["N"]:
                 tense_voice_mood = row["ccat-parse"][1:4]
-                form_list = forms.setdefault(lemma, {}).setdefault(tense_voice_mood, [])
+                form_list = forms.setdefault(lemma, {}).setdefault(tense_voice_mood, {}).setdefault("forms", [])
                 if {"form": form} not in form_list:
                     form_list.append({"form": form})
             elif mood in ["I", "D", "S", "O"]:
                 tense_voice_mood = row["ccat-parse"][1:4]
                 person_number = row["ccat-parse"][0] + row["ccat-parse"][5]
-                form_list = forms.setdefault(lemma, {}).setdefault(tense_voice_mood, {}).setdefault(person_number, [])
+                form_list = forms.setdefault(lemma, {}).setdefault(tense_voice_mood, {}).setdefault(person_number, {}).setdefault("forms", [])
                 if {"form": form} not in form_list:
                     form_list.append({"form": form})
             elif mood in ["P"]:
                 tense_voice_mood = row["ccat-parse"][1:4]
                 gender = row["ccat-parse"][6]
                 case_number = row["ccat-parse"][4:6]
-                form_list = forms.setdefault(lemma, {}).setdefault(tense_voice_mood, {}).setdefault(gender, {}).setdefault(case_number, [])
+                form_list = forms.setdefault(lemma, {}).setdefault(tense_voice_mood, {}).setdefault(gender, {}).setdefault(case_number, {}).setdefault("forms", [])
                 if {"form": form} not in form_list:
                     form_list.append({"form": form})
             else:
                 print >>sys.stderr, "*** can't handle mood {}".format(mood)
         elif lexeme["pos"] in "P":
-            form_list = forms.setdefault(lemma, [])
+            form_list = forms.setdefault(lemma, {}).setdefault("forms", [])
             if {"form": form} not in form_list:
                 form_list.append({"form": form})
     else:
@@ -68,16 +68,18 @@ for form, metadata in sorted(forms.items(), key=lambda x: collator.sort_key(x[0]
                 for case_number in ["NS", "AS", "GS", "DS", "VS", "NP", "AP", "GP", "DP", "VP"]:
                     if case_number in metadata[gender]:
                         print "        {}:".format(case_number)
-                        for form in metadata[gender][case_number]:
-                            print "            -"
-                            print "                form: {}".format(form["form"].encode("utf-8"))
+                        print "            forms:"
+                        for form in metadata[gender][case_number]["forms"]:
+                            print "                -"
+                            print "                    form: {}".format(form["form"].encode("utf-8"))
     elif pos in ["RP1"]:
         for case_number in ["NS", "AS", "GS", "DS", "VS", "NP", "AP", "GP", "DP", "VP"]:
             if case_number in metadata:
                 print "    {}:".format(case_number)
-                for form in metadata[case_number]:
-                    print "        -"
-                    print "            form: {}".format(form["form"].encode("utf-8"))
+                print "        forms:"
+                for form in metadata[case_number]["forms"]:
+                    print "            -"
+                    print "                form: {}".format(form["form"].encode("utf-8"))
     elif pos in ["V"]:
         for tense_voice_mood in [
             "AAN", "AMN", "APN",
@@ -87,9 +89,10 @@ for form, metadata in sorted(forms.items(), key=lambda x: collator.sort_key(x[0]
         ]:
             if tense_voice_mood in metadata:
                 print "    {}:".format(tense_voice_mood)
-                for form in metadata[tense_voice_mood]:
-                    print "        -"
-                    print "            form: {}".format(form["form"].encode("utf-8"))
+                print "        forms:"
+                for form in metadata[tense_voice_mood]["forms"]:
+                    print "            -"
+                    print "                form: {}".format(form["form"].encode("utf-8"))
         for tense_voice_mood in [
             "AAI", "AAS", "AAD", "AAO",
             "AMI", "AMS", "AMD", "AMO",
@@ -115,9 +118,10 @@ for form, metadata in sorted(forms.items(), key=lambda x: collator.sort_key(x[0]
                 for person_number in ["1S", "2S", "3S", "1P", "2P", "3P"]:
                     if person_number in metadata[tense_voice_mood]:
                         print "        {}:".format(person_number)
-                        for form in metadata[tense_voice_mood][person_number]:
-                            print "            -"
-                            print "                form: {}".format(form["form"].encode("utf-8"))
+                        print "            forms:"
+                        for form in metadata[tense_voice_mood][person_number]["forms"]:
+                            print "                -"
+                            print "                    form: {}".format(form["form"].encode("utf-8"))
         for tense_voice_mood in [
             "AAP", "AMP", "APP",
             "FAP", "FMP", "FPP",
@@ -132,14 +136,16 @@ for form, metadata in sorted(forms.items(), key=lambda x: collator.sort_key(x[0]
                         for case_number in ["NS", "AS", "GS", "DS", "VS", "NP", "AP", "GP", "DP", "VP"]:
                             if case_number in metadata[tense_voice_mood][gender]:
                                 print "            {}:".format(case_number)
-                                for form in metadata[tense_voice_mood][gender][case_number]:
-                                    print "                -"
-                                    print "                    form: {}".format(form["form"].encode("utf-8"))
+                                print "                forms:"
+                                for form in metadata[tense_voice_mood][gender][case_number]["forms"]:
+                                    print "                    -"
+                                    print "                        form: {}".format(form["form"].encode("utf-8"))
 
     elif pos in "P":
-        for form in metadata:
-            print "    -"
-            print "        form: {}".format(form["form"].encode("utf-8"))
+        print "    forms:"
+        for form in metadata["forms"]:
+            print "        -"
+            print "            form: {}".format(form["form"].encode("utf-8"))
     else:
         print >>sys.stderr, "*** can't handle pos {}".format(pos)
 
