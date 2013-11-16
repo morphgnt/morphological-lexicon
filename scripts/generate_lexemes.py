221d52e5f766d320edf190a4cadf4acc442465d2
James Tauber <jtauber@jtauber.com>
2013-11-16 11:09:02 +0800

    added 2Pe with BDAG and script to help generate new lexeme entries

---
+++ scripts/generate_lexemes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from morphgnt import filesets
+from morphgnt.utils import load_yaml
+
+lexemes = load_yaml("lexemes.yaml")
+fs = filesets.load("filesets.yaml")
+
+def convert(ccat, robinson):
+    pair = (ccat, robinson.split("-")[0])
+    if pair == ("N-", "N"):
+        return "N"
+    elif pair == ("V-", "V"):
+        return "N"
+    elif pair == ("A-", "A"):
+        return "A"
+    elif pair == ("D-", "ADV"):
+        return "D"
+    elif pair == ("P-", "PREP"):
+        return "P"
+    elif pair == ("RD", "D"):
+        return "RD"
+    elif pair == ("C-", "CONJ"):
+        return "C"
+    else:
+        return "{} {}".format(ccat, robinson)
+
+
+for row in fs["sblgnt-lexemes"].rows():
+    if row["lemma"].decode("utf-8") not in lexemes:
+        print "{}:".format(row["lemma"])
+        print "    pos: {}".format(convert(row["ccat-pos"], row["robinson"]))
