cc767210d18fd9244ec2d37d8771cc6dc9c796af
James Tauber <jtauber@jtauber.com>
2013-12-25 01:24:11 +0800

    in progress generate differences script

---
+++ scripts/generate_differences.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+
+from morphgnt.utils import load_yaml, sorted_items
+
+lexemes = load_yaml("lexemes.yaml")
+
+fully_match = 0
+
+METADATA_NAMES = [
+    "bdag-headword",
+    "danker-entry",
+    "dodson-entry",
+    "mounce-headword",
+]
+
+for lexeme, metadata in sorted_items(lexemes):
+
+    def r(metadata_name):
+        v = metadata.get(metadata_name)
+        if v:
+            v = v.split(",")[0]
+        return v if v != lexeme else None
+
+    differences = {
+        k: r(k) for k in METADATA_NAMES
+    }
+
+    if any(differences.values()):
+        print "{}:".format(lexeme.encode("utf-8"))
+    else:
+        fully_match += 1
+
+print >>sys.stderr, "{} fully-match".format(fully_match)
