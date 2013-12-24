df78a5f04a3d152aeb9ce758861054fba34eebd5
James Tauber <jtauber@jtauber.com>
2013-12-24 14:29:51 +0800

    whitespace tweaks

--- scripts/check_pos.py
+++ scripts/check_pos.py
@@ -11,38 +11,38 @@ lexemes = load_yaml("lexemes.yaml")
 regexes = [
     # verbs
     r"V@V@v-[0-9a-z\(\)]+$",
-    
+
     # verbs missing in dodson
     r"V@None@v-[0-9a-z\(\)]+$",
-    
+
     # verbs missing in morphcat
     r"V@V@None$",
-    
+
     # verbs with unknown morphcat
     r"V@V@\?\?$", # @@@
-    
+
     # verbs missing in dodson and morphcat
     r"V@None@None$",
-    
+
     # verbs with two morphcats
     r"V@V@v-[0-9a-z\(\)]+; v-[0-9a-z\(\)]+$", # @@@
-    
+
     # compound verbs
     r"V@V@cv-[0-9a-z\(\)]+$",
-    
+
     # compound verbs missing in dodson
     r"V@None@cv-[0-9a-z\(\)]+$",
-    
+
     # compound verbs missing full morphcat
     r"V@V@cv-$", # @@@
     r"V@None@cv-$", # @@@
-    
+
     # compound verbs that dodson thinks can be adjectives
     r"V@A,V@cv-[0-9a-z\(\)]+$", # @@@
-    
+
     # verbs that morphcat thinks is an adjective
     r"V@V@a-1a\(1\)$", # @@@
-    
+
     # indeclinable verb
     r"V/ARAM@None@None$", # @@@
     r"V/ARAM@ARAM@n-3g\(2\)$", # @@@
@@ -51,19 +51,19 @@ regexes = [
     # ἰδού/ἴδε
     r"X/V@V@\?\?$", # @@@
     r"X/V@INJ@None$", # @@@
-    
+
     # adjectives
     r"A@A@a-[0-9a-z\(\)]+$",
-    
+
     # adjective missing in dodson
     r"A@None@a-[0-9a-z\(\)]+$",
-    
+
     # adjective missing in morphcat
     r"A@A@None$",
-    
+
     # adjective missing in dodson and morphcat
     r"A@None@None$",
-    
+
     # adjectives that morphcat has as nouns
     r"A@A@n-3f\(2a\)$", # @@@
     r"A@None@n-3g\(2\)$", # @@@
@@ -74,7 +74,7 @@ regexes = [
     r"A@ADV-S@a-1a\(1\)$", # @@@
     r"A/ADV-S@ADV-S@None$", # @@@
     r"A/ADV@ADV@adverb$", # @@@
-    
+
     # adjectives that tisch has as adverbs
     r"A/ADV-S\?@A@a-1a\(2a\)$", # @@@
     r"A/ADV-C\?@None@None$", # @@@
@@ -94,12 +94,12 @@ regexes = [
     r"A@A@n-3g\(2\)$", # @@@
 
     r"A@A@n-3c\(2\)$", # @@@
-    
+
     # adjective / adverb conflation
     r"A/ADV@ADV@\['a-1a\(1\)', 'adverb'\]$", # @@@
     r"A/ADV-C@ADV-C@None$", # @@@
     r"A/ADV-C@ADV@None$", # @@@
-    
+
     # nouns
     r"N@N:M@n-1a$", # @@@
     r"N@N:F@n-1a$", # @@@
@@ -123,12 +123,12 @@ regexes = [
     r"N@N:M@n-2d\(1\)$",
     r"N@N:M@n-2e$", # @@@
     r"N@N:F@n-2e$", # @@@
-    
+
     # 3rd declension nouns
     r"N@N:M@n-3[0-9a-z\(\)]+$",
     r"N@N:F@n-3[0-9a-z\(\)]+$",
     r"N@N:N@n-3[0-9a-z\(\)]+$",
-    
+
     r"N@N-OI@n-3c\(6b\)$",
     r"N@N-OI@n-3g\(2\)$",
 
@@ -168,20 +168,20 @@ regexes = [
     r"N@N:F,N-PRI@n-1a$", # @@@
 
     r"\['N', 'X'\]@\['N:M', 'PRT'\]@None$", # @@@
-    
+
     # nouns missing in dodson
     r"N@None@n-[0-9a-z\(\)]+$", # @@@
-    
+
     # nouns missing in morphcat
     r"N@N:M@None$",
     r"N@N:N@None$",
     r"N@N:F@None$",
     r"N@N:M@\?\?$",
-    
+
     # nouns missing in dodson and morphcat
     r"N@None@None$",
-    r"N@None@\?\?$", 
-    
+    r"N@None@\?\?$",
+
     # noun / adjective / cross-over conflation
     r"A@N:F@n-1a$", # @@@
     r"A@N:M@n-1f$", # @@@
@@ -214,57 +214,57 @@ regexes = [
 
     r"N/ADV-K@ADV-K@adverb$", # @@@
     r"N/ADV-K@ADV-K@None$", # @@@
-    
+
     # article
     r"RA@T@a-1a\(2b\)$",
-    
+
     # demonstratives
     r"RD@D@a-1a\(2b\)$",
-    
+
     # reciprocal pronoun
     r"RP/C@C@a-1a\(2b\)$", # @@@
-    
+
     # reflexive pronouns
     r"RP/F-2@F@a-1a\(2b\)$", # @@@
     r"RP1/F@F@a-1a\(2a\)$", # @@@
     r"RP2/F@F@a-1a\(2b\)$", # @@@
-    
+
     # interrogative pronoun
     r"RI/X@I@a-4b\(2\)$", # @@@
     r"RI/A@A@a-1a\(2a\)$", # @@@
     r"RI@I@a-1a\(1\)$", # @@@
-    
+
     # correlative pronoun
     r"RR/K@K@a-1a\(2a\)$", # @@@
     r"RR/K@K,R@a-1a\(1\)$", # @@@
-    
+
     # correlative OR interrogative pronoun
     r"RI/Q@Q@a-1a\(2a\)$", # @@@
-    
+
     # personal pronouns
     r"RP@P@a-1a\(2b\)$", # @@@
     r"RP1@P@a-5$", # @@@
     r"RP2@P@a-5$", # @@@
     r"RP1/P-K@P@a-5$", # @@@
-    
+
     # relative pronouns
     r"RR@R@a-1a\(2b\)$",
-    
+
     # possessive pronouns
     r"A/RP1@S@a-1a\(2a\)$", # @@@
     r"A/S1@S@a-1a\(1\)$", # @@@
     r"A/S-2S@S@a-1a\(2a\)$", # @@@
     r"A/S-2P@S@a-1a\(1\)$", # @@@
-    
+
     # indefinite pronoun
     r"RI/X@X@a-4b\(2\)$", # @@@
     r"RI/A@A@a-1a\(1\)$", # @@@
-    
+
     r"C@CONJ@conj$",
     r"D@ADV@adverb$",
     r"P@PREP@prep$",
     r"X@PRT@particle$",
-    
+
     r"C@CONJ@particle$", # @@@
     r"C/ADV@ADV@adverb$", # @@@
     r"C/ADV@ADV-N@adverb$", # @@@
@@ -283,14 +283,14 @@ regexes = [
     r"C/CONJ-N@CONJ-N@conj$", # @@@
     r"C/D@ADV@adverb$", # @@@
     r"C/ADV@ADV@adverb; co$", # @@@
-    
+
     # adverbs missing in dodson
     r"D@None@adverb$",
     r"D/@@None@adverb$", # @@@
-    
+
     # adverbs missing in morphcat
-    r"D@ADV@None$", 
-    
+    r"D@ADV@None$",
+
     # adverbs missing in both dodson and morphcat
     r"D@None@None$",
 
@@ -305,7 +305,7 @@ regexes = [
     r"D@ADV,ADV-C@adverb$", # @@@
     r"D@ADV@prep$", # @@@
     r"D@ADV@adverb; pr$", # @@@
-    
+
     r"D@None@particle$", # @@@
     r"D/CONJ-N@CONJ-N@particle$", # @@@
     r"D/CONJ@CONJ@adverb$", # @@@
@@ -335,7 +335,7 @@ regexes = [
     r"P@ADV,PREP@adverb$", # @@@
     r"P/ADV@ADV@\?\?$", # @@@
     r"P/ADV@None@None$", # @@@
-    
+
     r"X@None@None$",
     r"X/HEB@HEB@particle$", # @@@
     r"X/COND@COND@conj$", # @@@
@@ -362,13 +362,13 @@ for lexeme, metadata in sorted_items(lexemes):
     pos = metadata.get("pos")
     dodson_pos = metadata.get("dodson-pos")
     morphcat = metadata.get("mounce-morphcat")
-    
+
     matched = False
     for compiled_regex in compiled_regexes:
         if compiled_regex.match("{}@{}@{}".format(pos, dodson_pos, morphcat)):
             matched = True
             break
-    
+
     total += 1
     if matched:
         match += 1
--- scripts/nfkc.py
+++ scripts/nfkc.py
@@ -5,5 +5,5 @@ import sys
 from morphgnt.utils import nfkc_normalize
 
 with open(sys.argv[1]) as f:
-	for line in f:
-		sys.stdout.write(nfkc_normalize(line.decode("utf-8")).encode("utf-8"))
+    for line in f:
+        sys.stdout.write(nfkc_normalize(line.decode("utf-8")).encode("utf-8"))
