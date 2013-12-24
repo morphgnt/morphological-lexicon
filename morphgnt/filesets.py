71f023c11eb80b9afdcf7f454a61005c5dc10358
James Tauber <jtauber@jtauber.com>
2013-12-24 14:27:01 +0800

    whitespace tweaks

--- morphgnt/filesets.py
+++ morphgnt/filesets.py
@@ -4,15 +4,15 @@ from .utils import load_yaml
 
 
 class FileSet(object):
-    
+
     def __init__(self, setname, metadata):
         self.setname = setname
         self.metadata = metadata
-    
+
     def files(self):
         for filename in self.metadata["files"]:
             yield os.path.abspath(os.path.join(self.metadata["prefix"], filename))
-    
+
     def rows(self):
         for filename in self.files():
             with open(filename) as f:
