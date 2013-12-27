3882b9768c1ad4fac8bb2f34e9310ccf8f91c9c0
James Tauber <jtauber@jtauber.com>
2013-12-27 23:14:10 +0800

    first ἀ- priv rule

--- scripts/analyze_components.py
+++ scripts/analyze_components.py
@@ -11,9 +11,11 @@ danker = load_yaml("../data-cleanup/danker-concise-lexicon/components.yaml")
 for lexeme, metadata in sorted_items(danker):
     components = metadata["components"]
 
-    if re.match(ur"^Heb\.$", components):
+    if re.match(ur"Heb\.$", components):
         pass
-    elif re.match(ur"^Heb\. ‘[^’]+’$", components):
+    elif re.match(ur"Heb\. ‘[^’]+’$", components):
+        pass
+    elif re.match(ur"ἀ- priv\., [\u0370-\u03FF\u1F00-\u1FFF]+; ‘[^’]+’$", components):
         pass
     else:
         print lexeme
