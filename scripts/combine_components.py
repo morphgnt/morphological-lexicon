eee64375f8f55c24e0a262d22de182b6360d3256
James Tauber <jtauber@jtauber.com>
2013-12-27 22:19:04 +0800

    loading of greenlee components

--- scripts/combine_components.py
+++ scripts/combine_components.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
 from morphgnt.utils import load_yaml
+from morphgnt.utils import nfkc_normalize as n
 
 danker = load_yaml("../data-cleanup/danker-concise-lexicon/components.yaml")
+
+greenlee = {}
+with open("../data-cleanup/greenlee-morphology/morphemes-utf8.txt") as f:
+    for line in f:
+        key, value = line.strip().split("\t")
+        greenlee[n(key.decode("utf-8").split(",")[0])] = {
+            "full-entry": n(key.decode("utf-8")),
+            "components": n(value.decode("utf-8")),
+        }
