d1e3f3de010132d867cd27a9ed7f901fe133dd4d
James Tauber <jtauber@jtauber.com>
2013-12-27 22:30:05 +0800

    print components and full entries

--- scripts/combine_components.py
+++ scripts/combine_components.py
@@ -12,12 +12,18 @@ greenlee = {}
 with open("../data-cleanup/greenlee-morphology/morphemes-utf8.txt") as f:
     for line in f:
         key, value = line.strip().split("\t")
-        greenlee[key.decode("utf-8").split(",")[0]] = {
-            "full-entry": key.decode("utf-8"),
-            "components": value.decode("utf-8"),
+        greenlee[n(key.decode("utf-8")).split(",")[0]] = {
+            "full-entry": n(key.decode("utf-8")),
+            "components": n(value.decode("utf-8")),
         }
 
 words = [n(word) for word in set(danker.keys()).union(set(greenlee.keys()))]
 
 for word in sorted(words, key=collator.sort_key):
     print "{}:".format(word.encode("utf-8"))
+    if word in danker:
+        print "    danker-full-entry: \"{}\"".format(danker[word]["full-entry"].encode("utf-8"))
+        print "    danker-components: \"{}\"".format(danker[word]["components"].encode("utf-8"))
+    if word in greenlee:
+        print "    greenlee-full-entry: \"{}\"".format(greenlee[word]["full-entry"].encode("utf-8"))
+        print "    greenlee-components: \"{}\"".format(greenlee[word]["components"].encode("utf-8"))
