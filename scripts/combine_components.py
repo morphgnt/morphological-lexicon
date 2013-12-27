c78d9628b37674353ff315d9058ccecd1f325dfa
James Tauber <jtauber@jtauber.com>
2013-12-27 22:25:54 +0800

    sort merged words

--- scripts/combine_components.py
+++ scripts/combine_components.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from pyuca import Collator
+collator = Collator()
+
 from morphgnt.utils import load_yaml
 from morphgnt.utils import nfkc_normalize as n
 
@@ -9,7 +12,12 @@ greenlee = {}
 with open("../data-cleanup/greenlee-morphology/morphemes-utf8.txt") as f:
     for line in f:
         key, value = line.strip().split("\t")
-        greenlee[n(key.decode("utf-8").split(",")[0])] = {
-            "full-entry": n(key.decode("utf-8")),
-            "components": n(value.decode("utf-8")),
+        greenlee[key.decode("utf-8").split(",")[0]] = {
+            "full-entry": key.decode("utf-8"),
+            "components": value.decode("utf-8"),
         }
+
+words = [n(word) for word in set(danker.keys()).union(set(greenlee.keys()))]
+
+for word in sorted(words, key=collator.sort_key):
+    print "{}:".format(word.encode("utf-8"))
