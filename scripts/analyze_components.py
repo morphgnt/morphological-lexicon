d37bd39aa5c3a2aa380c6f1ffcc6f64a7f5b3db6
James Tauber <jtauber@jtauber.com>
2013-12-28 00:16:07 +0800

    moved regexes to list

--- scripts/analyze_components.py
+++ scripts/analyze_components.py
@@ -5,19 +5,24 @@ import re
 
 from morphgnt.utils import load_yaml, sorted_items
 
+regexes = [
+    ur"Heb\.$",
+    ur"Heb\. ‘[^’]+’",
+    ur"ἀ- priv\., [\u0370-\u03FF\u1F00-\u1FFF]+; ‘[^’]+’",
+]
 
 danker = load_yaml("../data-cleanup/danker-concise-lexicon/components.yaml")
 
 for lexeme, metadata in sorted_items(danker):
     components = metadata["components"]
 
-    if re.match(ur"Heb\.$", components):
-        pass
-    elif re.match(ur"Heb\. ‘[^’]+’$", components):
-        pass
-    elif re.match(ur"ἀ- priv\., [\u0370-\u03FF\u1F00-\u1FFF]+; ‘[^’]+’$", components):
-        pass
-    else:
+    matched = False
+    for regex in regexes:
+        if re.match(regex, components):
+            matched = True
+            break
+
+    if not matched:
         print lexeme
         print components
         break
