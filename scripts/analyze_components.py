fbaa14fae2b20978c701490517da73f2d4977f8a
James Tauber <jtauber@jtauber.com>
2013-12-29 11:49:58 +0800

    (rather specific) new component regex

--- scripts/analyze_components.py
+++ scripts/analyze_components.py
@@ -12,7 +12,7 @@ regexes = [
     ur"Heb\. ‘[^’]+’$",
     ur"Heb\., of uncertain etymology$",
     ur"ἀ- priv\., [\u0370-\u03FF\u1F00-\u1FFF]+; ‘[^’]+’$",
-
+    ur"ἀ- priv\., [\u0370-\u03FF\u1F00-\u1FFF]+ =[\u0370-\u03FF\u1F00-\u1FFF]+ ‘[^’]+’ [^;]+; ‘[^’]+’$",
 ]
 
 compiled_regexes = [re.compile(regex) for regex in regexes]
