a2c758295afdf2eaae52fd96afec0155795ab493
James Tauber <jtauber@jtauber.com>
2015-11-13 23:22:32 -0500

    big clean up of categories and endings

--- projects/nominal_distinguishers/make_noun_lexicon.py
+++ projects/nominal_distinguishers/make_noun_lexicon.py
@@ -22,8 +22,12 @@ LEMMA_OVERRIDE = {
     "μήν": "μήν/N",
 }
 
+LEMMATIZATION_OVERRIDES = {
+    ("ῥιπτούντων", "-PAPGPM-"): "ῥιπτέω",
+}
+
 MOUNCE_OVERRIDES = {
-    "ἄγαμος": "n=2a/2b",
+    "ἄγαμος": ["n-2a", "n-2b"],
     "αἴτιον": "a-1a(1)",  # should it be αἴτιος?
     # "ἀκριβέστερον",
     "ἄκων": "a-2a(2)",
@@ -32,22 +36,24 @@ MOUNCE_OVERRIDES = {
     "ἄλλος": "a=1a(2b-HOS)",
     "ἀλλήλων": "a=1a(2b-HOS)",  # @@@
     # "ἀλυπότερος",
+    "ἀποστυγέω": "cv-1d(2a)",
     "ἀμφιέννυμι": "v-3c(1)",
     # "ἀνάθεμα",  # @@@ lemmatization bug?
     # "ἀνώτερος",
     "ἄπειμι": "cv-6b",
+    "ἀροτριάω": "v-1d(1)",
     "αὐτός": "a=1a(2b-HOS)",
     "ἄψινθος": "n-2b",
-    "βάθος": "n-3d(2b)",
+    "βάθος": "n-3d(2)",
     "βασίλειον": "a-3a",  # should it be βασίλειος in Luke?
-    "βάτος": "n=2a/2b",
+    "βάτος": ["n-2a", "n-2b"],
     # "βέλτιον",
     "γυνή": "n=3b(1GUNH)",
     "δάκρυον": "n=2c(SIN)",  # n-2c with σι(ν) DPN
     "δεῖνα": "n-3f(1a)",
     "δεκάτη": "a-1a(2a)",  # should it be δέκατος
     "δεσμόν": "n-2c",
-    "διάκονος": "n=2a/2b",
+    "διάκονος": ["n-2a", "n-2b"],
     "διαπλέω": "cv-1a(7)",
     "δοῦλος": "a-1a(2a)",  # @@@
     "δύο": "a-5(DUO)",
@@ -56,36 +62,39 @@ MOUNCE_OVERRIDES = {
     "εἰδωλόθυτον": "a-3a",  # should it be εἰδωλόθυτος ?
     "εἷς": "a=4b(2-EIS)",
     "εἴωθα": "v-1b(3)",
-    "ἑκατοντάρχης": "n=1f/n-2a",  # lemmatization issue?
+    "ἑκατοντάρχης": ["n-1f", "n-2a"],  # lemmatization issue?
     "ἐκεῖνος": "a=1a(2b-HOS)",
     "ἑκών": "a-2a(2)",  # made up subcategory to select n-3c(5b) in masculine/neuter
     "ἔννυχα": "a-3a",  # incorrect lemma?
     "ἐξανάστασις": "n-3e(5b)",
-    "ἐξανάστασις": "n-3e(5b)",
+    "ἐξουθενέω": "cv-1d(2a)",
+    "ἐπιποθέω": "cv-1d(2a)",
+    "ἐπιτελέω": "cv-1d(2b)",
     "ἔρημος": "a-3a",
-    "ἔρις": "n=3c(2-ERIS)",
+    "ἔρις": ["n=3c(2NU)", "n-3c(2)"],
     "ἔσθω": "v-1b(3)",
     "ἑτεροζυγέω": "v-1d(2a)",
     # "εὐκοπώτερος",
     "εὐπάρεδρον": "a-3a",  # should it be εὐπάρεδρος?
-    "ζῆλος": "n=2a/3d(2)",
+    "ζῆλος": ["n-2a", "n-3d(2)"],
     "ζυγός": "n-2a",
     # "ἥδιστα",
-    "ἦχος": ["n=2a/3d(2)"],
+    "ἦχος": ["n-2a", "n-3d(2)"],
     # "θάμβος",  # n-2a but neut
-    "θάμβος": "n-3d(2b)",
-    "θεός": "n=2a/2b",
+    "θάμβος": "n-3d(2)",
+    "θεός": ["n-2a", "n-2b"],
     "θέρμη": "n-1b",
-    "θυρωρός": "n=2a/2b",
+    "θυρωρός": ["n-2a", "n-2b"],
+    "ἱμάς": "n=3c(5a-ALPHA)",
     "Ἰουνία": "n-1a",
     "ἱππικόν": "a-1a(2a)",  # should it be ἱππικός?
     "Ἰσκαριώτης": "n-1f",
     "κἀκεῖνος": "a=1a(2b-HOS)",
     "κακοποιός": "a-3a",
     "κάμηλος": "n-2b",
-    "κλείς": "n=3c(2KLEIS)",
+    "κλείς": ["n=3c(2NU)", "n-3c(2)"],
     "λεπτόν": "a-1a(2a)",  # @@@ lemma?
-    "λιμός": "n=2a/2b",
+    "λιμός": ["n-2a", "n-2b"],
     "Μαθθαῖος": "n-2a",
     "Μαθθίας": "n-1d",
     "μάταιος": "a=1a(1)/a-3",
@@ -95,26 +104,32 @@ MOUNCE_OVERRIDES = {
     "μήν": "n-3f(1a)",
     "μητρολῴας": "n-1d",
     "μίγνυμι": "v-3c(2)",
+    "νῆστις": "n-3e(5a)",  # Mounce gives n-3c(2) with n-3e(5a) as footnote (typo'd as n-3c(5a)) but in NT it's only latter
     "νηφάλιος": "a-3a",  # @@@
     "νουμηνία": "n-1a",
     "νύμφη": "n-1b",
+    "νύξ": "n=3c(1KT)",
     "ὁ": "a=1a(2b-HO)",
     "ὅς": "a=1a(2b-HOS)",
+    "ὀδούς": "n=3c(5a-OMICRON)",
     "οἶμαι": "v-1d(2c)",
-    "ὄνος": "n=2a/2b",  # masculine in Luke
+    "ὄνος": ["n-2a", "n-2b"],  # masculine in Luke
     "ὅσιος": "a-3a",  # @@@
-    "ὀστέον": "n-2d",
+    "ὀστέον": ["n=2d(a)", "n-2c"],
     "οὐδείς": "a=4b(2-EIS)",
     "οὖς": "n=3c(6c-OUS)",
     "οὗτος": "a=1a(2b-HOS)",
     "ὀψία": "n-1a",
     "παραπλήσιον": "a-1a(1)",
-    "παρθένος": "n=2a/2b",  # only masculine in Revelation
+    "παρθένος": ["n-2a", "n-2b"], # only masculine in Revelation
     "παρίστημι": "cv-6a",
     "Πέργαμος": "n-2b",
-    "πλοῦτος": "n=2a(PLOUTOS)",
+    "πλοῦτος": ["n-2a", "n=2a(PLOUTOS)"],
     "πολύς": "a=1a(2a-POLUS)",
+    "πούς": ["n=3c(2-OD)", "n-3c(2)"],
+    "προφῆτις": ["n=3c(1NU)", "n-3c(1)"],
     "πυκνά": "a-1a(2a)",  # change to πυκνός in Luke?
+    "ῥιπτέω": "v-1d(2)",
     "σάββατον": "n=2c(SAB)",  # n-2c with σι(ν) DPN
     "σεαυτοῦ": "a=1a(2b-HOS)",  # @@@
     "σκῦλα": "n-2c",  # change to σκῦλον?
@@ -139,6 +154,7 @@ MOUNCE_OVERRIDES = {
     "ὕαλος": "n-2a",
     # "ὕστερον",
     "φοβέομαι": "v-1d(2a)",  # ?
+    "χάρις": ["n=3c(1NU)", "n-3c(1)"],
     "χείμαρρος": "n-2a",
     "χείρ": "n=3f(2a-XEIR)",
     "χρύσεος": "a-1b",  # should it be χρύσους?
@@ -179,6 +195,9 @@ for book_num in range(1, 28):
         if lemma[0].lower() != lemma[0]:
             continue
 
+        if (norm, ccat_parse) in LEMMATIZATION_OVERRIDES:
+            lemma = LEMMATIZATION_OVERRIDES[(norm, ccat_parse)]
+
         if lemma in LEMMA_OVERRIDE:
             lemma = LEMMA_OVERRIDE[lemma]
         if lemma in MOUNCE_OVERRIDES:
@@ -204,7 +223,7 @@ for book_num in range(1, 28):
         for cat in mounce_cat:
             mounce_by_lemma[lemma].add(cat)
 
-        new_mounce_cat = []
+        new_mounce_cat = set()
         for cat in mounce_cat:
             if cat == "a-1a(1)":
                 cat = {
@@ -215,7 +234,7 @@ for book_num in range(1, 28):
             elif cat == "a=1a(1)/a-3":
                 cat = {
                     "M": "n-2a",
-                    "F": "n=1a/2b",
+                    "F": ["n-1a", "n-2b"],
                     "N": "n-2c",
                 }[gender]
             elif cat == "a-1a(2a)":
@@ -239,20 +258,20 @@ for book_num in range(1, 28):
             elif cat == "a=1a(2b-HO)":
                 cat = {
                     "M": "n=2a(HO)",
-                    "F": "n=1b(HO)",
+                    "F": "n-1b",
                     "N": "n=2c(HO)",
                 }[gender]
             elif cat == "a=1a(2b-HOS)":
                 cat = {
-                    "M": "n=2a(HOS)",  # can get rid of? @@@
-                    "F": "n=1b(HOS)",  # can get rid of? @@@
-                    "N": "n=2c(HOS)",
+                    "M": "n-2a",
+                    "F": "n-1b",
+                    "N": "n=2c(HO)",
                 }[gender]
             elif cat == "a=1a(2b-HOSb)":
                 cat = {
                     "M": "n-2a",
                     "F": "n-1b",
-                    "N": "n=2c(HOSb)",
+                    "N": ["n=2c(HO)", "n-2c"],
                 }[gender]
             elif cat == "a-1b":
                 cat = {
@@ -262,9 +281,9 @@ for book_num in range(1, 28):
                 }[gender]
             elif cat == "a-2a":
                 cat = {
-                    "M": "n-3c(5a)",
+                    "M": "n=3c(5a-ALPHA)",
                     "F": "n-1c",
-                    "N": "n-3c(5a)",
+                    "N": "n=3c(5a-ALPHA)",
                 }[gender]
             elif cat == "a-2a(2)":  # made up
                 cat = {
@@ -293,13 +312,13 @@ for book_num in range(1, 28):
             elif cat == "a-3b(1)":
                 cat = {
                     "M": "n-2a",
-                    "F": "n=1a/2b",
+                    "F": ["n-1a", "n-2b"],
                     "N": "n-2c",
                 }[gender]
             elif cat == "a-3b(2)":
                 cat = {
                     "M": "n-2a",
-                    "F": "n=1b/2b",
+                    "F": ["n-1b", "n-2b"],
                     "N": "n-2c",
                 }[gender]
             elif cat == "a-4a":
@@ -329,16 +348,47 @@ for book_num in range(1, 28):
             elif cat == "a=4b(2-EIS)":
                 cat = {
                     "M": "n-3f(EIS)",  # made up
-                    "F": "n=1a(EIS)",
+                    "F": "n-1a",
                     "N": "n-3f(EIS)",
                 }[gender]
             elif cat.startswith("v") or cat.startswith("cv"):
                 if aspect_voice == "PA":
-                    cat = {
-                        "M": "n-3c(5-PAP)",  # made up
-                        "F": "n-1c",
-                        "N": "n-3c(5-PAP)",
-                    }[gender]
+                    if lemma in ["δίδωμι", "μεταδίδωμι", "παραδίδωμι", "ἀποδίδωμι"]:
+                        cat = {
+                            "M": "n=3c(5a-OMICRON)",  # made up
+                            "F": "n-1c",
+                            "N": "n=3c(5a-OMICRON)",
+                        }[gender]
+                    elif lemma in ["ἐπιδείκνυμι", "ὑποζώννυμι", "ἀποδείκνυμι"]:
+                        cat = {
+                            "M": "n=3c(5a-UPSILON)",  # made up
+                            "F": "n-1c",
+                            "N": "n=3c(5a-UPSILON)",
+                        }[gender]
+                    elif lemma in ["ἐπιτίθημι", "συνίημι", "τίθημι", "ἀνίημι", "μετατίθημι"]:
+                        cat = {
+                            "M": "n=3c(5a-EPSILON)",  # made up
+                            "F": "n-1c",
+                            "N": "n=3c(5a-EPSILON)",
+                        }[gender]
+                    elif cat in ["v-1d(2a)", "v-1d(2b)", "v-1d(3)", "cv-1d(2a)", "v-1b(4)", "cv-1d(2)", "cv-1d(2b)", "cv-1a(7)", "cv-1d(3)", "v-1d(2)"]:
+                        cat = {
+                            "M": "n=3c(5b-OU)",  # made up
+                            "F": "n-1c",
+                            "N": "n=3c(5b-OU)",
+                        }[gender]
+                    elif cat in ["v-1d(1a)", "v-1d(1b)", "cv-1d(1a)", "v-1d(1)", "cv-1d(1b)"]:
+                        cat = {
+                            "M": "n=3c(5b-W)",  # made up
+                            "F": "n-1c",
+                            "N": "n=3c(5b-W)",
+                        }[gender]
+                    else:
+                        cat = {
+                            "M": "n-3c(5b)",  # made up
+                            "F": "n-1c",
+                            "N": "n-3c(5b)",
+                        }[gender]
                 elif aspect_voice == "PM":
                     cat = {
                         "M": "n-2a",
@@ -359,16 +409,23 @@ for book_num in range(1, 28):
                     }[gender]
                 elif aspect_voice == "AP":
                     cat = {
-                        "M": "n-3c(5a-APP)",  # made up
+                        "M": "n=3c(5a-EPSILON)",  # made up
                         "F": "n-1c",
-                        "N": "n-3c(5a-APP)",
+                        "N": "n=3c(5a-EPSILON)",
                     }[gender]
                 elif aspect_voice == "XA":
-                    cat = {
-                        "M": "n-3c(1-XAP)",  # made up
-                        "F": "n-1c",
-                        "N": "n-3c(1-XAP)",
-                    }[gender]
+                    if lemma in ["ἵστημι", "παρίστημι", "συνίστημι", "περιΐστημι", "ἐφίστημι", "ἐνίστημι", "προΐστημι"]:
+                        cat = {
+                            "M": ["n=3c(1-XAP-OMEGA)", "n-3c(1-XAP)"],  # made up
+                            "F": "n-1c",
+                            "N": ["n=3c(1-XAP-OMEGA)", "n-3c(1-XAP)"],
+                        }[gender]
+                    else:
+                        cat = {
+                            "M": "n-3c(1-XAP)",  # made up
+                            "F": "n-1c",
+                            "N": "n-3c(1-XAP)",
+                        }[gender]
                 elif aspect_voice == "XM":
                     cat = {
                         "M": "n-2a",
@@ -377,9 +434,9 @@ for book_num in range(1, 28):
                     }[gender]
                 elif aspect_voice == "FA":
                     cat = {
-                        "M": "n-3c(5-PAP)",
+                        "M": "n-3c(5b)",
                         "F": "n-1c",
-                        "N": "n-3c(5-PAP)",
+                        "N": "n-3c(5b)",
                     }[gender]
                 elif aspect_voice == "FM":
                     cat = {
@@ -396,7 +453,11 @@ for book_num in range(1, 28):
                 else:
                     assert False, aspect_voice
 
-            new_mounce_cat.append(cat)
+            if isinstance(cat, str):
+                new_mounce_cat.add(cat)
+            else:  # list
+                for c in cat:
+                    new_mounce_cat.add(c)
 
         orig_norm = norm
         norm = strip_accents(norm)
@@ -426,12 +487,11 @@ for book_num in range(1, 28):
                 quit()
 
             if norm.endswith(ending.replace(".", "")):
-                success = True
+                success = set()
                 for cat in new_mounce_cat:
-                    if not re.match(class_regex, cat):
-                        success = False
-                        break
-                if success is True:
+                    if re.match(class_regex, cat):
+                        success.add(cat)
+                if success:
                     break
 
         if not success:
@@ -449,15 +509,18 @@ for book_num in range(1, 28):
         if aspect_voice == "--":
             assert orig_norm == theme + orig_ending, (orig_norm, theme, orig_ending)
             theme_by_lemma[lemma][gender].add(strip_accents(theme))
-            forms_by_lemma[lemma][gender][case_number].add((orig_norm, theme, orig_ending, explanation))
+            forms_by_lemma[lemma][gender][case_number].add((orig_norm, theme, orig_ending, explanation, "∨".join(success)))
         else:
             theme_by_lemma[lemma][aspect_voice + gender].add(strip_accents(theme))
-            forms_by_lemma[lemma][aspect_voice + gender][case_number].add((orig_norm, theme, orig_ending, explanation))
+            forms_by_lemma[lemma][aspect_voice + gender][case_number].add((orig_norm, theme, orig_ending, explanation, "∨".join(success)))
 
 
 collator = Collator()
 
 for k in sorted(forms_by_lemma.keys(), key=collator.sort_key):
+
+    # YAML
+
     print("{}:".format(k))
     print("    mounce: {}".format(", ".join(sorted(mounce_by_lemma[k]))))
     print("    forms:")
@@ -469,7 +532,13 @@ for k in sorted(forms_by_lemma.keys(), key=collator.sort_key):
             ))
             for case_number in ["NS", "GS", "DS", "AS", "VS", "NP", "VP", "GP", "DP", "AP"]:
                 if case_number in forms_by_lemma[k][gender]:
-                    print("            {}: {}".format(case_number, " / ".join("{} {}|{} {}".format(n, t, e1, "" if not e2 else e2) for n, t, e1, e2 in sorted(forms_by_lemma[k][gender][case_number]))))
+                    print("            {}: {}".format(
+                        case_number,
+                        " / ".join(
+                            "{} {}|{} {}".format(
+                                n, t, e1, "" if not e2 else e2
+                            ) for n, t, e1, e2, m in sorted(forms_by_lemma[k][gender][case_number]))))
+
     for aspect_voice in ["PA", "PM", "AA", "AM", "AP", "FA", "FM", "FP", "XA", "XM"]:
         printed_aspect_voice_yet = False
         for gender in ["M", "F", "N", "-"]:
@@ -488,4 +557,4 @@ for k in sorted(forms_by_lemma.keys(), key=collator.sort_key):
                             " / ".join(
                                 "{} {}|{} {}".format(
                                     n, t, e1, "" if not e2 else e2
-                                ) for n, t, e1, e2 in sorted(forms_by_lemma[k][aspect_voice + gender][case_number]))))
+                                ) for n, t, e1, e2, m in sorted(forms_by_lemma[k][aspect_voice + gender][case_number]))))
