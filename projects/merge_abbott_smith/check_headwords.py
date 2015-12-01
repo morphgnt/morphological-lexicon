4e3dbfd872bc12d11d8949be98e4f72dfc0ea9f0
James Tauber <jtauber@jtauber.com>
2015-12-01 22:12:12 +0800

    regex heuristics plus corrections to second column

--- projects/merge_abbott_smith/check_headwords.py
+++ projects/merge_abbott_smith/check_headwords.py
@@ -2,24 +2,38 @@
 
 import re
 
+S1_REGEX = [
+    r"\w+$",
+    r"(\w+)/(\w+)$",
+    r"(\w+)\(ν\)$",
+]
+
+S2_REGEX = [
+    r"G\d+$",
+    r"\?$",
+    r"G\d+(,G\d+)+$",
+]
+
 with open("abbott_smith_headwords.txt") as f:
     for line in f:
         s = line.strip().split("|")
-
-        S1_REGEX = [
-            r"\w+$",
-            r"(\w+)/(\w+)$",
-            r"(\w+)\(ν\)$",
-        ]
 
         success = False
         for regex in S1_REGEX:
             if re.match(regex, s[0]):
                 success = True
                 break
-
         if not success:
-            print(s[0])
+            print("column 1:", s[1])
+            quit()
+
+        success = False
+        for regex in S2_REGEX:
+            if re.match(regex, s[1]):
+                success = True
+                break
+        if not success:
+            print("column 2:", s[1])
             quit()
 
 print("success!")
