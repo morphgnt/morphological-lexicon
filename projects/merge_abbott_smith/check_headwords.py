691591de10dbf5392dcaaa8b79259ada5196c860
James Tauber <jtauber@jtauber.com>
2015-12-01 22:07:43 +0800

    regex heuristics plus corrections to first column

--- projects/merge_abbott_smith/check_headwords.py
+++ projects/merge_abbott_smith/check_headwords.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
+import re
+
 with open("abbott_smith_headwords.txt") as f:
     for line in f:
         s = line.strip().split("|")
-        if len(s) != 3:
-            print(line.strip())
+
+        S1_REGEX = [
+            r"\w+$",
+            r"(\w+)/(\w+)$",
+            r"(\w+)\(ν\)$",
+        ]
+
+        success = False
+        for regex in S1_REGEX:
+            if re.match(regex, s[0]):
+                success = True
+                break
+
+        if not success:
+            print(s[0])
+            quit()
+
+print("success!")
